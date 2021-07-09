b66bd35ca36f3b7df6a0628b6c80b008c98d8518
Jeff Ballinger <kumite@Jeffs-MacBook-Pro.local>
2021-07-08 23:40:34 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -36,7 +36,7 @@ class CommentForm extends Component {
   
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     // console.log('Current state is: ' + JSON.stringify(values));
     // alert('Current state is: ' + JSON.stringify(values));
   }
@@ -110,7 +110,7 @@ class CommentForm extends Component {
     );
   }
 
-  function RenderComments({comments, addComment, campsiteId}) {
+  function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
       return (
         <div className="col-md-5 m-1">
@@ -130,7 +130,7 @@ class CommentForm extends Component {
             )
             })
           }
-          <CommentForm campsiteId={campsiteId} addComment={addComment} />
+          <CommentForm campsiteId={campsiteId} postComment={postComment} />
         </div>
 
       );
@@ -176,7 +176,7 @@ function CampsiteInfo(props) {
           <RenderCampsite campsite={props.campsite} />
           <RenderComments
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
           />
         </div>
